d86499642ed70941c92fbd776d008968aabaaaf6
yoonjoonseok <yjsygm@naver.com>
2022-11-21 15:40:11 +0900

    다리의 길이를 입력 받는다.

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -4,5 +4,8 @@ public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+    	InputView inputView = new InputView();
+    	int bridgeSize = inputView .readBridgeSize();
+    	return;
     }
 }
--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,5 +1,7 @@
 package bridge;
 
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -9,7 +11,15 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+    	String input = Console.readLine();
+    	if(input.matches("-?\\d+")) 
+    		throw new IllegalArgumentException("[ERROR] 정수가 아닙니다.");
+    	
+    	int result = Integer.parseInt(input);
+    	if(result<3||20<result)
+    		throw new IllegalArgumentException("[ERROR] 3~20 사이의 숫자가 아닙니다.");
+    	
+        return result;
     }
 
     /**
