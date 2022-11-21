01f42af52e856effc5772e66694474d4de79b1b6
yoonjoonseok <yjsygm@naver.com>
2022-11-21 15:59:04 +0900

    입력 받은 길이만큼의 다리를 생성한다.

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,11 +1,19 @@
 package bridge;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
 
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
-    	InputView inputView = new InputView();
-    	int bridgeSize = inputView .readBridgeSize();
-    	return;
-    }
+	public static void main(String[] args) {
+		// TODO: 프로그램 구현
+		InputView inputView = new InputView();
+		int bridgeSize = inputView.readBridgeSize();
+
+		BridgeNumberGenerator bridgeNumberGenerator = null;
+		BridgeMaker bridgeMaker = new BridgeMaker(bridgeNumberGenerator);
+		List<String> bridgeUD = bridgeMaker.makeBridge(bridgeSize);
+		
+		return;
+	}
 }
--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -1,5 +1,6 @@
 package bridge;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -7,17 +8,28 @@ import java.util.List;
  */
 public class BridgeMaker {
 
-    private final BridgeNumberGenerator bridgeNumberGenerator;
+	private final BridgeNumberGenerator bridgeNumberGenerator;
 
-    public BridgeMaker(BridgeNumberGenerator bridgeNumberGenerator) {
-        this.bridgeNumberGenerator = bridgeNumberGenerator;
-    }
+	public BridgeMaker(BridgeNumberGenerator bridgeNumberGenerator) {
+		this.bridgeNumberGenerator = bridgeNumberGenerator;
+	}
 
-    /**
-     * @param size 다리의 길이
-     * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
-     */
-    public List<String> makeBridge(int size) {
-        return null;
-    }
+	/**
+	 * @param size 다리의 길이
+	 * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
+	 */
+	public List<String> makeBridge(int size) {
+		int zeroOne = 0;
+		List<String> result = new ArrayList<>();
+		
+		for (int i = 0; i < size; i++) {
+			zeroOne = bridgeNumberGenerator.generate();
+			if (zeroOne == 1)
+				result.add("U");
+			if (zeroOne == 0)
+				result.add("D");
+		}
+
+		return result;
+	}
 }
